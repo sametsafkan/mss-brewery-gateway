3359ab30c1c31ec6c7ee266ccbeaeb54560aeec4
Samet Safkan <sametsafkan@gmail.com>
2020-05-02 11:29:15 +0300

    configured routes on service discovery.

--- src/main/java/com/sametsafkan/mssbrewerygateway/config/GoogleConfig.java
+++ src/main/java/com/sametsafkan/mssbrewerygateway/config/GoogleConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-//@Configuration
-//@Profile("google")
+@Configuration
+@Profile("google")
 public class GoogleConfig {
 
     @Bean
--- src/main/java/com/sametsafkan/mssbrewerygateway/config/LoadBalancedRoutes.java
+++ src/main/java/com/sametsafkan/mssbrewerygateway/config/LoadBalancedRoutes.java
@@ -6,21 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!local-discovery")
+@Profile("local-discovery")
 @Configuration
-public class LocalHostRouteConfig {
+public class LoadBalancedRoutes {
 
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder){
+    public RouteLocator buildLoadBalancedRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*","/api/v1/beer/*","/api/v1/beerUpc/*")
-                        .uri("http://localhost:8080")
+                        .uri("lb://mss-beer-service")
                         .id("beer-service"))
                 .route(r -> r.path("/api/v1/customers/*/orders*", "/api/v1/customers/*/orders/*", "/api/v1/customers/*/customers*")
-                        .uri("http://localhost:8081")
+                        .uri("lb://beer-order-service")
                         .id("beer-order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory", "/api/v1/inventory*", "/api/v1/inventory/*")
-                        .uri("http://localhost:8082")
+                        .uri("lb://beer-inventory-service")
                         .id("beer-inventory-service"))
                 .build();
     }
